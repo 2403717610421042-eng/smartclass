316fd9bbf38b3db98acce0bde855342103e52be8
raja <2403717610421042@cit.edu.in>
2026-06-17 23:46:15 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -85,7 +85,15 @@ app.post("/reset/:id", async (req, res) => {
 
 // Start Server
 const PORT = process.env.PORT || 3000;
+const path = require("path");
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/admin.html", (req, res) => {
+    res.sendFile(path.join(__dirname, "admin.html"));
+});
 app.listen(PORT, () => {
     console.log(`Server Running on Port ${PORT}`);
 });
